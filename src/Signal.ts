af7cc3e66b61670e172f543fa330b6a9b1fefa07
Zach Brockway <neverender@gmail.com>
2013-10-06 23:23:04 -0700

    TypeScript 0.9 compatibility fixes

--- src/Signal.ts
+++ src/Signal.ts
@@ -35,7 +35,7 @@ class Signal {
     * @type Any
     * @private
     */
-    private _prevParams = null;
+    private _prevParams: any = null;
 
     /**
     * Signals Version Number
@@ -51,31 +51,31 @@ class Signal {
     * already dispatched before.
     * @type boolean
     */
-    public memorize: bool = false;
+    public memorize: boolean = false;
 
     /**
     * @type boolean
     * @private
     */
-    private _shouldPropagate: bool = true;
+    private _shouldPropagate: boolean = true;
 
     /**
     * If Signal is active and should broadcast events.
     * <p><strong>IMPORTANT:</strong> Setting this property during a dispatch will only affect the next dispatch, if you want to stop the propagation of a signal use `halt()` instead.</p>
     * @type boolean
     */
-    public active: bool = true;
+    public active: boolean = true;
 
     /**
     * @method validateListener
     * @param {Any} listener
     * @param {Any} fnName
     */
-    public validateListener(listener, fnName) {
+    public validateListener(listener: Function, fnName: String) {
 
         if (typeof listener !== 'function')
         {
-            throw new Error('listener is a required param of {fn}() and should be a Function.'.replace('{fn}', fnName));
+            throw new Error('listener is a required param of {' + fnName + '}() and should be a Function.');
         }
 
     }
@@ -88,7 +88,7 @@ class Signal {
     * @return {SignalBinding}
     * @private
     */
-    private _registerListener(listener, isOnce: bool, listenerContext, priority: number): SignalBinding {
+    private _registerListener(listener, isOnce: boolean, listenerContext, priority: number): SignalBinding {
 
         var prevIndex: number = this._indexOfListener(listener, listenerContext);
         var binding: SignalBinding;
@@ -166,7 +166,7 @@ class Signal {
     * @param {Object} [context]
     * @return {boolean} if Signal has the specified listener.
     */
-    public has(listener, context?: any = null): bool {
+    public has(listener, context: any = null): boolean {
 
         return this._indexOfListener(listener, context) !== -1;
 
@@ -179,7 +179,7 @@ class Signal {
     * @param {Number} [priority] The priority level of the event listener. Listeners with higher priority will be executed before listeners with lower priority. Listeners with same priority level will be executed at the same order as they were added. (default = 0)
     * @return {SignalBinding} An Object representing the binding between the Signal and listener.
     */
-    public add(listener, listenerContext?: any = null, priority?: number = 0): SignalBinding {
+    public add(listener: Function, listenerContext: any = null, priority: number = 0): SignalBinding {
 
         this.validateListener(listener, 'add');
 
@@ -194,7 +194,7 @@ class Signal {
     * @param {Number} [priority] The priority level of the event listener. Listeners with higher priority will be executed before listeners with lower priority. Listeners with same priority level will be executed at the same order as they were added. (default = 0)
     * @return {SignalBinding} An Object representing the binding between the Signal and listener.
     */
-    public addOnce(listener, listenerContext?: any = null, priority?: number = 0): SignalBinding {
+    public addOnce(listener: Function, listenerContext: any = null, priority: number = 0): SignalBinding {
 
         this.validateListener(listener, 'addOnce');
 
@@ -208,7 +208,7 @@ class Signal {
     * @param {Object} [context] Execution context (since you can add the same handler multiple times if executing in a different context).
     * @return {Function} Listener handler function.
     */
-    public remove(listener, context?: any = null) {
+    public remove(listener: Function, context: any = null) {
 
         this.validateListener(listener, 'remove');
 
